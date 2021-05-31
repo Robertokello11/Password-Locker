37d25261b5219c5c2b04f2ec2b94ceed278f3381
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-06-01 00:11:32 +0300

    add all chnages made

--- user_Test.py
+++ user_Test.py
@@ -22,4 +22,27 @@ class TestUser(unittest.TestCase):
         '''
         self.assertEqual(self.new_user.username, "Robert")
         self.assertEqual(self.new_user.password, "Robert11")
- 
+
+     def test_save_user(self):
+        '''
+        confirm if the user information can be saved 
+        in the user list
+        '''
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+
+         # 3rd test ## saving users ##
+
+     def test_save_mutliple_users(self):
+        '''
+        check whether you can store more than one user
+        '''
+        self.new_user.save_user()
+        test_user = User("test", "password")
+        test_user.save_user()
+        self.assertEqual(len(User.user_list), 2)
+
+        #4th test## Delete user ##
+
+        
+   
