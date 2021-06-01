e52573a27a2e8342e2eb656921026a62ae911e91
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-06-01 10:29:39 +0300

    make the commits

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
 from credentials import Credentials
 from user import User
 import random
+
+#user create account#
+
+def create_useraccount(username, password):
+    '''
+    method creates a user account
+    '''
+    new_user = User(username, password)
+    return new_user
+
+#save the user#
+
