011eb0a5fba5368a4135748638158174c4abd973
Robertokello11 <robert.otieno@student.moringaschool.com>
2021-06-01 10:37:37 +0300

    make changes

--- credentials.py
+++ credentials.py
@@ -4,3 +4,9 @@ class Credentials:
     class that creates instaces of user accounts
     '''
     cred_list = []
+
+    def __init__(self, account , email , passlock):
+    
+        self.account = account
+        self.email = email
+        self.passlock = passlock
